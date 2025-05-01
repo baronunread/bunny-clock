b2ebce32577ee0f800532262f90917308c2fe18c
Andrea Bruno <50637199+baronunread@users.noreply.github.com>
2025-05-01 15:25:41 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// Ensure darkMode: 'class'
+module.exports = {
+  darkMode: 'class', // Make sure this is set to 'class'
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
